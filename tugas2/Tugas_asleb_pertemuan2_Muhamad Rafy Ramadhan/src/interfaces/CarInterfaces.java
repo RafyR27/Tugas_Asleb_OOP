7d6563fffe6b4d637b9d49765b4d923c90ea0949
Muhamad Rafy Ramadhan. <117496357+RafyR27@users.noreply.github.com>
2025-09-04 12:46:43 +0700

    Add files via upload

---
+++ tugas2/Tugas_asleb_pertemuan2_Muhamad Rafy Ramadhan/src/interfaces/CarInterfaces.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+/**
+ *
+ * @author Rafy1
+ */
+public interface CarInterfaces {
+    void displayCar();
+    void startEngine();
+    void Throttle();
+    void Brake();
+    void turnOnHeadLamp();
+    void turnOfHeadLamp();
+    
+}
